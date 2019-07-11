e2a3ba63b5dadded9206dabd149d6cb8479f822d
alarivan <alarivan@pm.me>
2019-07-11 16:04:02 -0700

    Add FixedForm component

---
+++ src/components/common/FixedForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <modal
+    :name="name"
+    width="100%"
+    height="auto"
+    :pivotY="pivotY"
+    @opened="$emit('opened')"
+    @closed="$emit('closed')"
+  >
+    <SimpleForm class="p-2" @submit="submit" @cancel="cancel">
+      <slot></slot>
+    </SimpleForm>
+  </modal>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+import SimpleForm from "Components/SimpleForm.vue";
+
+export default {
+  name: "component-fixed-form",
+  components: {
+    SimpleForm
+  },
+
+  data() {
+    return {};
+  },
+
+  props: {
+    name: { type: String, required: true }
+  },
+
+  methods: {
+    submit() {
+      this.$emit("submit");
+    },
+
+    cancel() {
+      this.close();
+
+      this.$emit("cancel");
+    },
+
+    open() {
+      this.$modal.show(this.name);
+    },
+
+    close() {
+      this.$modal.hide(this.name);
+    }
+  },
+
+  computed: {
+    pivotY() {
+      return this.$mq == "sm" ? 1 : 0.5;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.v--modal {
+  border-radius: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VModal from "vue-js-modal";
+import VueMq from "vue-mq";
 import App from "./App.vue";
 import store from "Store";
 import router from "Router";
@@ -9,6 +10,17 @@ import "Styles/main.scss";
 Vue.config.productionTip = false;
 
 Vue.use(VModal);
+
+Vue.use(VueMq, {
+  breakpoints: {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    xxl: Infinity
+  },
+  defaultBreakpoint: "sm"
+});
 
 new Vue({
   store,
