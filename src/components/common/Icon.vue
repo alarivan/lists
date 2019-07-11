8d1f6e7d1fc3f9604f622d291d3368a0f0a334bf
alarivan <alarivan@pm.me>
2019-07-11 11:20:21 -0700

    Add Icon component

---
+++ src/components/common/Icon.vue
@@ -0,0 +1,55 @@
+<template>
+  <svg class="icon" :class="sizeClass">
+    <use :xlink:href="href" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "component-icon",
+
+  props: {
+    href: { type: String, required: true },
+    size: { type: String }
+  },
+
+  computed: {
+    sizeClass() {
+      let result = "";
+      switch (this.size) {
+        case "md":
+          result = "icon-md";
+          break;
+        case "lg":
+          result = "icon-lg";
+          break;
+
+        default:
+          break;
+      }
+
+      return result;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.icon {
+  width: 1rem;
+  height: 1rem;
+  stroke-width: 0;
+  stroke: currentColor;
+  fill: currentColor;
+
+  &.icon-md {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+
+  &.icon-lg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+}
+</style>
