a79f47313e6ccee0c3461c8e8f54392fc4207752
alarivan <alarivan@pm.me>
2019-07-20 15:34:48 -0700

    Add ListName component

---
+++ src/components/List/ListName.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 data-cy="list-name" class="font-bold flex-auto p-2">{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "component-list-name",
+
+  props: {
+    name: { type: String, required: true }
+  }
+};
+</script>
+
+<style lang="scss"></style>
