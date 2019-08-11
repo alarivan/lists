d542b5bed485d79b3f4cc28b80a070615d061bc5
alarivan <alarivan@pm.me>
2019-08-11 16:18:35 -0700

    Remove unused styles

--- src/components/List/Items.vue
+++ src/components/List/Items.vue
@@ -79,32 +79,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.list-enter-active,
-.list-leave-active,
-.list-move {
-  transition: 300ms cubic-bezier(0.59, 0.12, 0.34, 0.95);
-  transition-property: opacity, transform;
-}
-
-.list-enter {
-  opacity: 0;
-  transform: translateX(50px) scaleY(0.5);
-}
-
-.list-enter-to {
-  opacity: 1;
-  transform: translateX(0) scaleY(1);
-}
-
-.list-leave-active {
-  position: absolute;
-  width: 100%;
-}
-
-.list-leave-to {
-  opacity: 0;
-  transform: scaleY(0);
-  transform-origin: center top;
-}
-</style>
+<style lang="scss"></style>
