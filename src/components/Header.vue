41e5adee76fe32ec3a6f135293ca91bf3dccfadf
alarivan <alarivan@pm.me>
2019-09-14 15:52:02 -0700

    Add Update to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="component-header flex justify-between mb-4 px-1">
     <Logo />
+    <Update />
     <Session />
   </div>
 </template>
@@ -8,13 +9,15 @@
 <script>
 import Session from "Components/Header/Session.vue";
 import Logo from "Components/Header/Logo.vue";
+import Update from "Components/Header/Update.vue";
 
 export default {
   name: "component-header",
 
   components: {
     Session,
-    Logo
+    Logo,
+    Update
   }
 };
 </script>
